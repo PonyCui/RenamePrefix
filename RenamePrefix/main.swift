188c97c3b469823b639cd92741c0cf72b81cdef2
PonyCui <cuis@vip.qq.com>
2017-02-15 14:05:22 +0800

    Initial Commit

---
+++ RenamePrefix/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RenamePrefix
+//
+//  Created by 崔明辉 on 2017/2/15.
+//  Copyright © 2017年 UED Center. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
