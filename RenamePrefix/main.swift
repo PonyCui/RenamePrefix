c1d9e6076871a573f9bb94a43f0a3c2c656a5320
PonyCui <cuis@vip.qq.com>
2017-02-15 14:22:00 +0800

    commit.

--- RenamePrefix/main.swift
+++ RenamePrefix/main.swift
@@ -8,5 +8,33 @@
 
 import Foundation
 
-print("Hello, World!")
+let env = ProcessInfo().environment["PWD"]!
+let from = ProcessInfo().arguments[1]
+let to = ProcessInfo().arguments[2]
+
+if let enumerator = FileManager.default.enumerator(atPath: env) {
+    for file in enumerator {
+        if let file = file as? String, (file.hasSuffix(".h") || file.hasSuffix(".m") || file.hasSuffix(".pbxproj")) {
+            let file = env + "/" + file
+            do {
+                let contents = try String(contentsOfFile: file)
+                let newContents = contents.replacingOccurrences(of: from, with: to)
+                let fileName = URL(fileURLWithPath: file).lastPathComponent
+                if fileName.hasPrefix(from) {
+                    try FileManager.default.removeItem(atPath: file)
+                    try newContents.write(toFile: file.replacingOccurrences(of: from, with: to),
+                                          atomically: true,
+                                          encoding: String.Encoding.utf8)
+                }
+                else {
+                    try newContents.write(toFile: file, atomically: true, encoding: String.Encoding.utf8)
+                }
+            }
+            catch {
+                continue
+            }
+        }
+    }
+}
+
 
